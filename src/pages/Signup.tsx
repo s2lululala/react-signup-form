dc3b13dfd0ac6467f9d97025a3485cd72b0dbff3
Soohyun Choi <djm05190@gmail.com>
2022-05-31 17:47:52 +0900

    ✨ Add password element

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 
 function Signup() {
   const [email, setEmail] = useState("");
@@ -10,6 +10,12 @@ function Signup() {
   const [isValidPhone, setIsValidPhone] = useState(false);
   const [phoneCheck, setPhoneCheck] = useState(false);
   const phoneReg = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
+
+  const [password, setPassword] = useState("");
+  const [repassword, setRepassword] = useState("");
+  const [isValidPassword, setIsValidPassword] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState(false);
+  const passwordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
 
   function checkEmail() {
     console.log(`Email is ${email}`);
@@ -24,7 +30,10 @@ function Signup() {
   return(
     <div>
       <div>
-        이메일
+        * 표시된 항목은 필수로 입력해야합니다
+      </div>
+      <div>
+        이메일*
         <div>
           <input
             type="text"
@@ -37,7 +46,7 @@ function Signup() {
         </div>
       </div>
       <div>
-        전화번호
+        전화번호*
         <div>
           <input
             type="text"
@@ -49,7 +58,27 @@ function Signup() {
         </div>
       </div>
       <div>
-        <button disabled={emailCheck ? false : true}>제출</button>
+        비밀번호* (비밀번호는 8자 이상, 영문 대,소문자, 숫자를 포함해야합니다)
+        <div>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {setPassword(e.target.value); passwordReg.test(e.target.value) ? setIsValidPassword(true) : setIsValidPassword(false); repassword === e.target.value ? setPasswordCheck(true) : setPasswordCheck(false);}}
+          />
+          {isValidPassword ? "" : "비밀번호 형식에 맞지 않습니다"}
+        </div>
+        비밀번호 확인*
+        <div>
+          <input
+            type="password"
+            value={repassword}
+            onChange={(e) => {setRepassword(e.target.value); password === e.target.value ? setPasswordCheck(true) : setPasswordCheck(false);}}
+          />
+          {passwordCheck ? "" : "비밀번호가 일치하지 않습니다"}
+        </div>
+      </div>
+      <div>
+        <button disabled={emailCheck && phoneCheck && isValidPassword && passwordCheck? false : true}>제출</button>
       </div>
     </div>
   )
