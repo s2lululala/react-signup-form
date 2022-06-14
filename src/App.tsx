9e8114eabc311c478975657609fb0018902b2176
Soohyun Choi <djm05190@gmail.com>
2022-06-14 17:31:34 +0900

    ✨ Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Greeting from "./pages/Greeting";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
@@ -6,11 +6,20 @@ import Signup from "./pages/Signup";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/signup" element={<Signup/>}></Route>
-        <Route path="/greeting" element={<Greeting/>}></Route>
-      </Routes>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <header>
+          <Link to='/'>
+            <div>
+              SIGNUP FORM
+            </div>
+          </Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/signup" element={<Signup/>}></Route>
+          <Route path="/greeting" element={<Greeting/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
